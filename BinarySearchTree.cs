5706639f087e26be2a99263afc4ce3ec9529fd82
GokulVelavan <gokulvelavans@gmail.com>
2021-11-21 22:04:49 +0530

    size of the tree

---
+++ BinarySearchTree.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+        public BinarySearchTree(T nodeData)  // constructor 
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData; //creation of new node
+
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                {
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                }
+                else
+                    this.LeftTree.Insert(item); //adding in left tree
+            }
+            else
+            {
+                if (this.RightTree == null)
+                {
+                    this.RightTree = new BinarySearchTree<T>(item);
+                }
+                else
+                    this.RightTree.Insert(item); //adding in right tree
+            }
+        }
+
+        public void Display()//program to diplay the node data
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+
+        public void GetSize()
+        {
+            Console.WriteLine("\nSize" + " " + (1 + this.leftCount + this.rightCount));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,32 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to BST");
+            BinarySearchTree<int> obj1 = new BinarySearchTree<int>(56);
+            obj1.Insert(56);
+            obj1.Insert(30);
+            obj1.Insert(70);
+            obj1.Insert(22);
+            obj1.Insert(40);
+            obj1.Insert(11);
+            obj1.Insert(3);
+            obj1.Insert(16);
+            obj1.Insert(60);
+            obj1.Insert(95);
+            obj1.Insert(65);
+            obj1.Insert(63);
+            obj1.Insert(67);
+            obj1.Display();
+            obj1.GetSize();
+
+
+
+
+
+
+
+
+
+
             Console.ReadLine();
         }
     }
